39ecc7f76279879e737e940234adc96705d5de66
Sarfraz Nawaz Shahnawaz <sshahna2@sundevils.asu.edu>
2024-03-30 03:23:22 -0700

    Added a shopping cart

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -64,6 +64,9 @@ function ProductList() {
                         <option key={category} value={category}>{category}</option>
                     ))}
                 </select>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cart">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                </svg>
             </div>
             <div className="grid-container">
                 {filteredProducts.map(product => (
@@ -80,8 +83,8 @@ function ProductList() {
                                 <p><strong>Rating: </strong>{product.rating}</p>
                             </div>
                             <br></br>
-                            <div className="button-container">
-                                <button>Add to Cart</button>
+                            <div>
+                                <button className="button-container">Add to Cart</button>
                             </div>
                         </div>
                     </div>
