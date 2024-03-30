cf6c8c3d954396776650e8cbf9c3a67707d8a9fd
Sarfraz Nawaz Shahnawaz <sshahna2@sundevils.asu.edu>
2024-03-30 02:05:42 -0700

    Aligned the Add to Cart button and added search button

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,6 +4,7 @@ import './ProductList.css';
 
 function ProductList() {
     const [products, setProducts] = useState([]);
+    const [searchItem, setSearchItem] = useState("");
 
     useEffect(() => {
         const fetchProducts = async()=> {
@@ -24,18 +25,34 @@ function ProductList() {
         console.log(products[0]);
     }, [products]);
 
+    const handleSearch = (event) => {
+        setSearchItem(event.target.value);
+    };
+
+    const filteredProducts = products.filter(product =>
+        product.title.toLowerCase().includes(searchItem.toLowerCase())
+    );
+
     return (
         <div className="product-list">
-            <h2>Available Items</h2>
+            <input
+                type="text"
+                placeholder="Search products..."
+                value={searchItem}
+                onChange={handleSearch}
+            />
             <div className="grid-container">
-                {products.map(product => (
+                {filteredProducts.map(product => (
                     <div key={product.id} className="product-card">
                         <img src={product.thumbnail} alt={product.title} />
                         <div className="product-details">
                             <h3>{product.title}</h3>
+                            <br></br>
                             <p><strong>Description: </strong>{product.description}</p>
+                            <br></br>
                             <p><strong>Price: ${product.price}</strong></p>
                             <p><strong>Rating: {product.rating}</strong></p>
+                            <br></br>
                             <div className="button-container">
                                 <button>Add to Cart</button>
                             </div>
