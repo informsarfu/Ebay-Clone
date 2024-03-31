9aa0dfb9d8c5f12af309bd8a627f92111195a535
Sarfraz Nawaz Shahnawaz <sshahna2@sundevils.asu.edu>
2024-03-30 17:01:13 -0700

    Added the shopping cart functionality

--- src/App.js
+++ src/App.js
@@ -4,32 +4,6 @@ import Header from './components/Header';
 import ProductList from './components/ProductList';
 
 function App() {
-  // const [data, setData] = useState(null);
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // const fetchData = () => {
-  //   fetch('https://dummyjson.com/auth/login', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({
-  //       username: 'kminchelle',
-  //       password: '0lelplR',
-  //       expiresInMins: 30
-  //     })
-  //   })
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     console.log(data);
-  //   })
-  //   .catch(error => {
-  //     console.error('Error logging in:', error);
-  //   });
-  // }
-  
-
   return (
     <div className="App">
       <Header/>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,14 @@
 import React, {useState,useEffect} from "react";
 import axios from "axios";
 import './ProductList.css';
+import './ShoppingCart.css';
 
 function ProductList() {
     const [products, setProducts] = useState([]);
     const [searchItem, setSearchItem] = useState("");
     const [selectedCategory, setSelectedCategory] = useState("");
     const [cart,setCart] = useState([]);
+    const [showCart, setShowCart] = useState(false);
 
     useEffect(() => {
         const fetchProducts = async()=> {
@@ -38,7 +40,12 @@ function ProductList() {
     };
 
     const handleCart = (item) => {
-        setCart([...cart,item]);
+        setCart(cart => [...cart,item]);
+        console.log("items",cart);
+    };
+
+    const toggleCart = () => {
+        setShowCart(!showCart);
     };
 
     const filteredProducts = products.filter(product => {
@@ -69,9 +76,21 @@ function ProductList() {
                         <option key={category} value={category}>{category}</option>
                     ))}
                 </select>
+                <div onClick={toggleCart}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="cart">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                 </svg>
+                </div>
+                {showCart && (
+                <div className="shopping-cart">
+                    <h2>Your Cart</h2>
+                    <ul>
+                        {cart.map((item, index) => (
+                            <li key={index}>{item.title}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
             </div>
             <div className="grid-container">
                 {filteredProducts.map(product => (
